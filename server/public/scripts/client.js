679d7e093831870b4be36c86cba7d7e980758ca7
Bryce Barsness <bryce5@gmail.com>
2021-01-23 22:07:50 -0600

    added get and render

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,3 +4,39 @@ $(document).ready(start);
 let itemList = [];
 let completedItemId = 0;
 
+// get the data from router using 'GET' method using '/tasks' route.
+// save the response as taskList then run render function with the taskList array.
+function getList() {
+  $.ajax({
+    method: "GET",
+    url: "/tasks",
+  })
+    .then((response) => {
+      taskList = response;
+      render(taskList);
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      console.warn("There was an error getting the list");
+    });
+}
+
+// clear the container on the dom and run a loop through the tasklist array.
+// append item with delete button and complete button on same row.
+// add 'complete' class if status is Y.
+function render() {
+  $(".js-container").empty();
+  for (let i = 0; i < taskList.length; i++) {
+    const item = taskList[i];
+    $(".js-container").append(`
+    <tr class="row">
+      <td class ="js-item">${item.item}</td>
+      <td><button class ="js-btn-complete" data-id="${item.id}" data-index="${i}">Complete</button></td>
+      <td><button class ="js-btn-delete" data-id="${item.id}">Delete</button></td>
+    </tr>`);
+    if (item.complete === "Y") {
+      const $el = $(".js-container").children().last();
+      $el.addClass("complete");
+    }
+  }
+}
--- server/routes/tasks.routes.js
+++ server/routes/tasks.routes.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+//get info from db and sent back to client
+router.get('/', (req, res) => {
+    let queryText = (`SELECT * FROM "tasks" ORDER BY "id" ASC;`)
+    pool.query(queryText)
+    .then(result => {
+        console.log(result);
+            res.send(result.rows);
+        }
+    ).catch((error) => {
+            console.log('error with get route', error);
+            res.sendStatus(500);
+        });
+    
+});
 
 
 
